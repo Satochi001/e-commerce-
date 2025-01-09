e20615e9474e6abf64dfbb39684d9f8ecd49156c
emy4dev <91508426+Satochi001@users.noreply.github.com>
2025-01-09 01:49:29 +0000

    storybook

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
@@ -26,5 +27,52 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+=======
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+>>>>>>> 3e95239 (storybook)
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+<<<<<<< HEAD
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+=======
+
+import './assets/main.css'
+
+createApp(App).mount('#app')
+>>>>>>> 3e95239 (storybook)
